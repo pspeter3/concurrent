9db3c994c4858e4707beec099f9ae0f09a366976
Phips Peter <pspeter333@gmail.com>
2013-08-26 23:22:15 -0700

    Adding concat grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,5 +16,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-gh-pages');
 };
