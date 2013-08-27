9d0c4935d33c15e4f0aff92603f5633917c089a4
Phips Peter <pspeter333@gmail.com>
2013-08-26 23:56:30 -0700

    Updating grunt to have default task generate browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,21 +9,28 @@ module.exports = function(grunt) {
     },
     concat: {
       options: {
-        banner: grunt.file.read('./browser/banner.jst'),
-        footer: grunt.file.read('./browser/footer.jst'),
+        banner: grunt.file.read('browser/banner.jst'),
+        footer: grunt.file.read('browser/footer.jst'),
         process: function(src) {
-          var noModules = src.replace('= module.exports =', '=')
+          var noModules = src.replace('= module.exports =', '=');
           return noModules.replace(/var .+? = require\(.+?\);/g, '');
         }
       },
       dist: {
         src: [
-          './lib/state.js',
-          './lib/promise.js',
-          './lib/future.js',
-          './lib/collections.js'
+          'lib/state.js',
+          'lib/promise.js',
+          'lib/future.js',
+          'lib/collections.js'
         ],
-        dest: './browser/concurrent.js'
+        dest: 'browser/concurrent.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'browser/concurrent.min.js': ['browser/concurrent.js']
+        }
       }
     },
     shell: {
@@ -39,8 +46,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-gh-pages');
 };
